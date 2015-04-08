d6b38447bbe38f92f8b31f1aae8efc9435751607
Geoff Williams <geoff.williams17@gmail.com>
2015-04-08 15:11:03 +0100

    Initial commmit from local git

---
+++ indoor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  indoor
+//
+//  Created by Geoff Williams on 08/04/2015.
+//  Copyright (c) 2015 Geoff Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ indoor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  indoor
+//
+//  Created by Geoff Williams on 08/04/2015.
+//  Copyright (c) 2015 Geoff Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
